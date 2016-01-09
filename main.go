a43527716b02dc22bd755f316ee4346779db7f54
Craig Peterson <cpeterson@stackoverflow.com>
2016-01-09 15:02:27 -0700

    testing realip

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/abiosoft/caddy-git"
 	"github.com/captncraig/caddy-cmd"
+	"github.com/captncraig/caddy-realip"
 	"github.com/captncraig/caddy-stats"
+	"github.com/captncraig/caddycustom/json"
 	"github.com/mholt/caddy/caddy"
 )
 
@@ -11,5 +13,7 @@ func main() {
 	caddy.RegisterDirective("git", git.Setup, "shutdown")
 	caddy.RegisterDirective("stats", stats.Setup, "shutdown")
 	caddy.RegisterDirective("cmd", cmd.Setup, "basicauth")
+	caddy.RegisterDirective("json", json.Setup, "templates")
+	caddy.RegisterDirective("realip", realip.Setup, "tls")
 	caddy.Main()
 }
